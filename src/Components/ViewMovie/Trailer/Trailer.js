24d5626964639100fc93ecd74a6750a463646015
casp-e-r <aswinkarooth@gmail.com>
2021-12-20 22:24:13 +0530

    react video moadal trailer added

--- src/Components/ViewMovie/Trailer/Trailer.js
+++ src/Components/ViewMovie/Trailer/Trailer.js
@@ -1,26 +1,40 @@
 import axios from '../../../axios'
 import React, { useEffect, useState } from 'react'
 import { API_KEY } from '../../../constants/constants'
+import ModalVideo from 'react-modal-video'
+import 'react-modal-video/css/modal-video.css'
+
 
 function Trailer({TvMovie,ID}) {
     const [video, setVideo] = useState([])
+    const [VideoModal, setVideoModal] = useState(false)
     useEffect(() => {
         axios.get(`/${TvMovie}/${ID}}/videos?api_key=${API_KEY}&language=en-US`).then(res=>{
             res.data.results.map(e=>{
                 if (e.type==='Trailer' || 'Teaser'  && e.site==='Youtube') {
-                    setVideo([...video,e])
+                    setVideo(e)
                 }
             })
         })
+        
     }, [ID,TvMovie])
+    
 
-    console.log(video[0].key.length)
+    // video[0] && console.log(video[0].key.length)
     
     return (
-        <div>
-            {console.log(video[0])}
+        <div className='Trailer-outer'>
+            {console.log(video)}
             <div>
-                <h1>s</h1>
+            <ModalVideo
+            channel="youtube"
+            modalVideo="movie-modal-video"
+            isOpen={VideoModal}
+            onClose={()=>setVideoModal(false)}
+            // playlist={!movie ? null : movie.videos.results[0] ? movie.videos.results.map(video => video.key) : null}
+            videoId={video.length!=0 && video.key}
+      />
+                <button onClick={()=>setVideoModal(true)}>s</button>
             </div>
         </div>
     )
